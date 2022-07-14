3e8f64be9b726d63c9e6dedbdcd7f984119f75af
Branko <branko.miric.ns@gmail.com>
2022-07-14 14:32:51 +0200

    created necessary components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { VotingComponent } from './voting/voting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "register",
+    component: RegistrationComponent
+  },
+  {
+    path: "vote",
+    component: VotingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
